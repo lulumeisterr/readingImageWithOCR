c76bc382158cf654ed78ec3c9df853686111ecb7
Lucas Rodrigues do Nascimento <LucasN@added.com.br>
2018-08-17 17:53:58 -0300

    script pronto

--- ImagemTest/src/br/com/added/test/Imag.java
+++ ImagemTest/src/br/com/added/test/Imag.java
@@ -1,26 +1,42 @@
 package br.com.added.test;
 
 import java.io.File;
-import net.sourceforge.tess4j.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
 
 public class Imag {
-	
-	
-	public static void main(String[] args) {
-		
-		File imageFile = new File("imagens/exame.jpg");
-	    ITesseract instance = new Tesseract();  
-	    instance.setDatapath("/ImagemTest/tessdata-master");
-	    instance.setLanguage("por.traineddata");
-	    
-	    try {
-	        String result = instance.doOCR(imageFile);
-	        System.out.println(result);
-	    } catch (TesseractException e) {
-	        System.err.println(e.getMessage());
-	    }
-	    
-		
+
+
+	public static void main(String[] args) throws IOException {
+
+		FileWriter arquivo;
+
+		File imageFile = new File("imagens/biot.jpg");
+
+		ITesseract instance = new Tesseract();  
+
+		instance.setDatapath("tessdata-master/tesseract-ocr");
+		instance.setLanguage("por");
+
+		try {
+
+			String result = instance.doOCR(imageFile);
+			String textoQueSeraEscrito = result;
+
+			arquivo = new FileWriter(new File("Arquivo.txt"));
+			arquivo.write(textoQueSeraEscrito);
+			arquivo.close();
+
+			System.out.println(result);
+
+		} catch (TesseractException e) {
+			System.err.println(e.getMessage());
+		}
+
 	}
 
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# readingImageWithOCR
-readingImageWithOCR
